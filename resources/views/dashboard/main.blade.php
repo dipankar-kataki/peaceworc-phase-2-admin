2bd24b2968fb10fdde84ebe6b9341c269d8cb3a3
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-08-23 17:03:50 +0530

    top cards

---
+++ resources/views/dashboard/main.blade.php
@@ -0,0 +1,9 @@
+@extends('welcome')
+@section('page-title', 'Dashboard')
+@section('custom-css')
+@endsection
+@section('content')
+    @include('common.overview-card')
+@endsection
+@section('custom-scripts')
+@endsection
