5d68022a18d56d2d26fbee709da9ce441c371d53
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-08-30 15:04:56 +0530

    become agency done

--- resources/views/common/sidebar-main.blade.php
+++ resources/views/common/sidebar-main.blade.php
@@ -81,7 +81,7 @@
                     <li><a class="slide-item" href="{{route('admin.get.manage.service.page')}}">Manage Services</a></li>
                     <li><a class="slide-item" href="{{route('admin.get.become.caregiver.page')}}">Manage Become Caregiver</a></li>
                     <li><a class="slide-item" href="{{route('admin.get.become.agency.page')}}">Manage Become Agency</a></li>
-                    <li><a class="slide-item" href="image-compare.html">Manage Testimonial</a></li>
+                    {{-- <li><a class="slide-item" href="image-compare.html">Manage Testimonial</a></li> --}}
                 </ul>
             </li>
            
