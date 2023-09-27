02c16d55e3d3fa3065ab2e4f755db2b88df116e5
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-09-27 12:50:40 +0530

    medical history

--- resources/views/agency/job/job-details.blade.php
+++ resources/views/agency/job/job-details.blade.php
@@ -23,8 +23,14 @@
                                             {{ $get_job_details->title ?? 'Not Found' }}
                                         </div>
                                         <hr>
+
+                                        <h6>Description:</h6>
+                                        <div class="description">
+                                            {{ $get_job_details->description ?? 'Not Found' }}
+                                        </div>
+                                        <hr>
                                         
-                                        <div class="d-flex flex-row justify-content-between align-items-center">
+                                        <div class="d-flex flex-row justify-content-between flex-wrap">
                                             <div class="job-timing">
                                                 <h6>Timing:</h6>
                                                 <p style="margin-bottom:2px;">Start Date & Time: 
@@ -39,26 +45,75 @@
                                             </div>
                                             <div class="job-care-type">
                                                 <h6>Care Type:</h6>
-                                                    {{$get_job_details->care_type}}
+                                                {{$get_job_details->care_type}}
                                             </div>
                                             <div class="job-care-items">
                                                 <h6>Care Items:</h6>
 
                                                     @php  
                                                         $data = json_decode($get_job_details->care_items, true);
-                                                        echo 'Age : ' . $data[0]['age']; echo ', '; echo 'Gender : ' . $data[0]['gender']; echo ', '; echo 'Patient Name : ' . $data[0]['patient_name'];
+                                                        foreach ($data as $key => $value) {
+                                                          echo 'Age: ' . $value['age']; echo ', '; echo 'Gender: ' . $value['gender']; echo ', '; echo 'Patient Name: ' . $value['patient_name']. "<br />"; 
+                                                        }
+                                                        
                                                     @endphp
                                             </div>
                                             <div class="job-amount">
                                                 <h6>Amount:</h6>
                                                 <span class="text-success">$</span> {{$get_job_details->amount}}
                                             </div>
+
+                                            <div class="job-adress">
+                                                <h6>Location:</h6>
+                                                {{'Appartment: '.$get_job_details->appartment_or_unit.', '}} {{'Floor: '.$get_job_details->floor_no.', '}} {{'Street: '.$get_job_details->street}}, {{'City: '.$get_job_details->city}}, {{'State: '.$get_job_details->state}}, <br /> {{'ZipCode: '.$get_job_details->zip_code}}, {{'Country: '.$get_job_details->country}}
+                                            </div>
+                                            
+                                        </div>
+                                        <hr>
+                                        <div class="d-flex flex-row justify-content-between">
+                                            <div class="medical-history">
+                                                <h6>Medical History:</h6>
+
+                                                @php  
+                                                    $data = json_decode($get_job_details->medical_history, true);
+                                                    if($data != null){
+                                                        foreach ($data as $key => $value) {
+                                                            if($key == 3 || $key == 6 || $key == 9 || $key == 12 || $key == 15){
+                                                                echo "<br />";
+                                                            } 
+                                                            echo ($key + 1). ') ' . '<span style="margin-right:10px; text-transform:capitalize;">'.$value.'</span>'; 
+                                                        }
+                                                    }else{
+                                                        echo 'Not Found';
+                                                    }
+                                                @endphp
+                                            </div>
+
+                                            <div class="medical-history">
+                                                <h6>Expertise:</h6>
+                                                @php  
+                                                    
+                                                    $data = json_decode($get_job_details->expertise, true);
+
+                                                    if($data != null){
+                                                        foreach ($data as $key => $value) {
+                                                            if($key == 3 || $key == 6 || $key == 9 || $key == 12 || $key == 15){
+                                                                echo "<br />";
+                                                            } 
+                                                            echo ($key + 1). ') ' . '<span style="margin-right:10px; text-transform:capitalize;">'.$value.'</span>'; 
+                                                        }
+                                                    }else{
+                                                       echo "Not Found";
+                                                    }   
+                                                @endphp
+                                                
+                                            </div>
                                             
                                         </div>
                                         
                                         
                                         {{-- <label class="main-content-label tx-13 mg-b-20">Contact Information</label>
-                                        <div class="main-profile-social-list d-flex flex-row flex-wrap align-items-center">
+                                        <div class="main-profile-social-list d-flex flex-row flex-wrap align-get_job_detailss-center">
                                             <div class="media">
                                                 <div class="media-icon bg-gray-100 text-primary">
                                                     <i class="fa fa-phone"></i>
@@ -97,7 +152,7 @@
                                         </div>
                                         <hr>
                                         <label class="main-content-label tx-13 mg-b-20">Other Information</label>
-                                        <div class="main-profile-social-list d-flex flex-row flex-wrap align-items-center">
+                                        <div class="main-profile-social-list d-flex flex-row flex-wrap align-get_job_detailss-center">
                                             <div class="media mr-4 mt-2">
                                                 <div class="media-icon bg-gray-100 text-primary">
                                                     <i class="fas fa-file-contract"></i>
@@ -109,7 +164,7 @@
                                             </div>
                                             <div class="media mr-4 mt-2">
                                                 <div class="media-icon bg-gray-100 text-success">
-                                                    <i class="fas fa-sitemap"></i>
+                                                    <i class="fas fa-sget_job_detailsap"></i>
                                                 </div>
                                                 <div class="media-body">
                                                     <span>Organization Type</span>
