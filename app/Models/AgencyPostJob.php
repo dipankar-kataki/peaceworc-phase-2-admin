0a44be4d66147febd72de0557112274ea4175ab2
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-12-05 18:01:04 +0530

    client profile

--- app/Models/AgencyPostJob.php
+++ app/Models/AgencyPostJob.php
@@ -22,37 +22,37 @@ class AgencyPostJob extends Model
     }
 
     public function getStatusAttribute($value){
-        if($value === 0){
+        if($value == 0){
             return 'Not Posted';
-        }else if($value === 1){
+        }else if($value == 1){
             return 'Open Job';
-        }else if($value === 2){
+        }else if($value == 2){
             return 'Ongoing Job';
-        }else if($value === 3){
+        }else if($value == 3){
             return 'Completed Job';
-        }else if($value === 4){
+        }else if($value == 4){
             return 'Closed';
-        }else if($value === 5){
+        }else if($value == 5){
             return 'Pending';
-        }else if($value === 6){
+        }else if($value == 6){
             return 'Bidding Started';
-        }else if($value === 7){
+        }else if($value == 7){
             return 'Bidding Ended';
-        }else if($value === 8){
+        }else if($value == 8){
             return 'Quick Call';
-        }else if($value === 9){
+        }else if($value == 9){
             return 'On Hold';
-        }else if($value === 10){
+        }else if($value == 10){
             return 'Upcoming';
-        }else if($value === 11){
+        }else if($value == 11){
             return 'Cancelled';
-        }else if($value === 12){
+        }else if($value == 12){
             return 'Expired';
-        }else if($value === 13){
+        }else if($value == 13){
             return 'Deleted';
-        }else if($value === 14){
+        }else if($value == 14){
             return 'Not Started';
-        }else if($value === 15){
+        }else if($value == 15){
             return 'Not Accepted';
         }
     }
